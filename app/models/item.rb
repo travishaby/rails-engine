7e2b030afed29bd081240c517baf160d670fc327
Travis Haby <travishaby@gmail.com>
2015-11-04 00:34:47 -0700

    changed models to have callbacks to convert csv unit prices to correct place value

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ActiveRecord::Base
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  before_save :convert_unit_price
+
+  def convert_unit_price
+    self.unit_price = self.unit_price/100
+  end
 end
