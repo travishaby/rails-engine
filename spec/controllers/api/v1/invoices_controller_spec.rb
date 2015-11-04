f2f78f3d4817a13c6e577d8dfe7fc2b73f6c6a76
Travis Haby <travishaby@gmail.com>
2015-11-04 09:57:19 -0700

    refactored invoices controller spec to use eager loading of let blocks

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::InvoicesController, type: :controller do
   context "happy paths for invoices controller" do
-    let(:invoice1) { create(:invoice) }
-    let(:invoice2) { create(:invoice,
+    let!(:invoice1) { create(:invoice) }
+    let!(:invoice2) { create(:invoice,
                               status: "pending",
                          merchant_id: invoice1.merchant_id) }
-    let(:invoice3) { create(:invoice,
+    let!(:invoice3) { create(:invoice,
                          merchant_id: invoice1.merchant_id,
                          customer_id: invoice1.customer_id) }
 
     it "should display all invoices" do
-      [invoice1, invoice2, invoice3]
       get :index, format: :json
 
       assert_response :success
@@ -25,21 +24,18 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should find a invoice by customer_id" do
-      [invoice1, invoice2]
       get :find, format: :json, customer_id: invoice1.customer_id
 
-      expect(parsed_body[:id]).to eq(invoice1.id)
+      expect(parsed_body[:id]).to eq(invoice3.id)
     end
 
     it "should find all invoices by merchant_id" do
-      [invoice1, invoice2, invoice3]
       get :find_all, format: :json, merchant_id: invoice1.merchant_id
 
       expect(parsed_body.size).to eq(3)
     end
 
     it "should return a random invoice" do
-      [invoice1, invoice2, invoice3]
       get :random, format: :json
       found_invoice = Invoice.find_by(status: parsed_body[:status])
 
