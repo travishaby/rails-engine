c3be3b0a9724cdc4558f96a291f20099762c4b7a
Travis Haby <travishaby@gmail.com>
2015-11-04 22:27:39 -0700

    all model tests implemented

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  context "valid invoice_item" do
+    it "has all attributes respective to its tabel columns" do
+      invoice_item = InvoiceItem.new(item_id: 1,
+                                  invoice_id: 1,
+                                    quantity: 10,
+                                  unit_price: 10.0)
 
+      expect(invoice_item).to be_valid
+    end
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
+  context "valid invoice" do
+    it "has all attributes respective to its tabel columns" do
+      invoice = Invoice.new(customer_id: 1,
+                            merchant_id: 1,
+                                 status: "pending")
 
+      expect(invoice).to be_valid
+    end
+  end
 end
